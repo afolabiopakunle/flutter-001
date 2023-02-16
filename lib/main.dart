8e0a668dc12f03c26d5f10e40d8122295e2dfed2
Afolabi <opakunleafolabi@gmail.com>
2023-02-16 10:27:06 +0100

    Organized Logical Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'My First Flutter App',
-    home: Scaffold(
-      appBar: AppBar(title: Text('My First App')),
-      body: new Material(
-        color: Colors.lightBlueAccent,
-        child: Center(
-            child: Text(
-          'Hello Flutter',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 40.0,
-          ),
-        )),
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app_screens/first_screen.dart';
+
+void main() => runApp(MyFlutterApp());
+
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My First Flutter App',
+      home: Scaffold(
+        appBar: AppBar(title: Text('My First App')),
+        body: FirstScreen()
       ),
-    ),
-  ));
+    );
+    throw UnimplementedError();
+  }
 }
