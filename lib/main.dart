fde46106589a82fe8f4f9c822d448f81c85006e0
Afolabi <opakunleafolabi@gmail.com>
2023-02-16 09:52:39 +0100

    app bar title added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-      child: Text(
-    'Hello Flutter',
-    textDirection: TextDirection.ltr,
-  )));
+  runApp(MaterialApp(
+    title: 'My First Flutter App',
+    home: Scaffold(
+      appBar: AppBar(title: Text('My First App')),
+      body: new Material(
+        color: Colors.lightBlueAccent,
+        child: Center(
+            child: Text(
+          'Hello Flutter',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 40.0,
+          ),
+        )),
+      ),
+    ),
+  ));
 }
